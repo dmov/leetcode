b5b60296c3de5d93b062df7fef85651c0d704997
dmov <dimnrty@mail.ru>
2023-04-19 19:59:31 +0300

    Time: 9 ms (65.58%), Space: 47 MB (82.20%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -6,14 +6,14 @@ class Solution {
         result.add(intervals[0]);
 
         for (int i = 1; i < intervals.length; i++) {
-            int[] previousInterval = result.get(result.size() - 1);
-            int[] currentInterval = intervals[i];
+            final int[] previousInterval = result.get(result.size() - 1);
+            final int[] currentInterval = intervals[i];
             if (previousInterval[1] >= currentInterval[0]) {
                 previousInterval[1] = Math.max(previousInterval[1], currentInterval[1]);
             } else {
                 result.add(currentInterval);
             }
         }
-        return result.toArray(new int[][]{});
+        return result.toArray(new int[result.size()][2]);
     }
 }
