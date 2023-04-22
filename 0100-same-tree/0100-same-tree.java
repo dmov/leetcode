4df519ffd9c90adefab5cd034de343f42267549f
dmov <dimnrty@mail.ru>
2023-04-22 20:25:32 +0300

    Time: 0 ms (100.00%), Space: 40.3 MB (41.99%) - LeetHub

---
+++ 0100-same-tree/0100-same-tree.java
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        final boolean isPNull = p == null;
+        final boolean isQNull = q == null;
+        if (isPNull && isQNull) {
+            return true;
+        } else if (isPNull || isQNull) {
+            return false;
+        }
+
+        return p.val == q.val && isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+}
